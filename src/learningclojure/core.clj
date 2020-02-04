f1483f055b48e51138460815767df13427bf875b
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-02-04 20:52:28 +0000

    Anonymous functions

--- src/learningclojure/core.clj
+++ src/learningclojure/core.clj
@@ -6,41 +6,63 @@
   [& args]
   (println "Hello, World!"))
 
-(do
-  (println "Hello")
-  (println "World"))
+;; (do
+;;   (println "Hello")
+;;   (println "World"))
 
-"hello"   ; string
-\e        ; character
-0xBC      ; hex
-#"[0-9]+" ; regex
-\newline  ; special character
+"hello"   ;; string
+\e        ;; character
+0xBC      ;; hex
+#"[0-9]+" ;; regex
+\newline  ;; special character
 
-nil         ; null val
-true false  ; bool
-:alpha      ; keyword
+nil         ;; null val
+true false  ;; bool
+:alpha      ;; keyword
 
-'(1 2 3)      ; list
-[1 2 3]       ; vector
-#{1 2 3}      ; set
-{:a 1, :b 2}  ; map
+'(1 2 3)      ;; list
+[1 2 3]       ;; vector
+#{1 2 3}      ;; set
+{:a 1, :b 2}  ;; map
 
 (+ 1 2)
 
-; Defining Functions
+;; Defining Functions
 (defn greet  [name]  (str "Hello, " name) )
 
-; Multi-arity Functions
+;; Multi-arity Functions
+;; Each arity is a list ([param*] body*) and one arity can invoke another
 (defn messenger 
   ([] (messenger "Hello world"))
   ([msg] (println msg))
 )
 
-; Variadic Functions
-; Functions may also define a variable number of parameters - this is known as a variadic function
+;; Variadic Functions
+;; Functions may also define a variable number of parameters - this is known as a variadic function
 
 (defn hello [greeting & who]
   (println greeting who))
 
-; Anonymous function
+;; Anonymous function
+
+(fn [msg] (println msg))
+
+;; Immediately Invoke an anonymouse func
 ((fn [message] (println message)) "Hello world!")
+
+;; Clojure only has expressions
+(defn f [name] (str "Hello " name))
+
+(def f (fn [name] str "Hello " name))
+
+;; The above two are equivalent
+
+;; Anonymous function syntax sugar
+;; Equivalent to: (fn [x] (+ 6 x))
+#(+ 6 %)
+
+;; Equivalent to: (fn [x y] (+ x y))
+#(+ %1 %2)
+
+;; Equivalent to: (fn [x y & zs] (println x y zs))
+#(println %1 %2 %&)
