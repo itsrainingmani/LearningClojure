d3f7dfecfbd5aac5b0cd171fdd8fe99c9795ceb3
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-04-30 01:07:06 -0500

    Complete all check knowledge tests for functions

--- src/learningclojure/core.clj
+++ src/learningclojure/core.clj
@@ -143,6 +143,62 @@ true false  ;; bool
 ;; ignores them all and returns 100
 (defn always-thing [& xs] 100)
 
+;; 6) Define a function make-thingy which takes a single arg x. Should return another function, which takes any number of args and always returns x
+(defn make-thingy [x] (fn [& xs] x))
+
+;; Tests for make-thingy
+(let [n (rand-int Integer/MAX_VALUE)
+      f (make-thingy n)]
+  (assert (= n (f)))
+  (assert (= n (f 123)))
+  (assert (= n (apply f 123 (range)))))
+
+;; This is the "constantly" function in Clojure
+
+;; 7) Define a function triplicate which takes another function and calls it 3 times without any args
+(defn triplicate [f]
+  (f) (f) (f))
+
+;; 8) Define a function opposite which takes a single argument f. It should return another function which takes any number of arguments, applies f on them, and then calls not on the result. The not function in Clojure does logical negation.
+(defn opposite [f]
+  (fn [& args] (not (apply f args))))
+
+;; 9) Define a function triplicate2 which takes another function and any number of arguments, then calls that function three times on those arguments. Re-use the function you defined in the earlier triplicate exercise.
+
+(defn triplicate2 [f & args]
+  triplicate (fn [] (apply f args)))
+
+;; 10) Using the java.lang.Math class (Math/pow, Math/cos, Math/sin, Math/PI), demonstrate the following mathematical facts:
+
+(assert (= (Math/cos Math/PI) -1.0))
+
+(defn sinx2cosx [x] (+ (Math/pow (Math/sin x) 2) (Math/pow (Math/cos x) 2)))
+
+;; 11) Define a function that takes an HTTP URL as a string, fetches that URL from the web, and returns the content as a string.
+
+(defn http-get [url]
+  (slurp
+   (.openStream
+    (java.net.URL. url))))
+
+;; Clojure's slurp interprets its argument as a URL first before trying it as a file name
+
+(defn http-get-simple [url] (slurp url))
+
+;; 12) Define a function one-less-arg that takes two arguments:
+;;  f, a function
+;;    x, a value
+;; and returns another function which calls f on x plus any additional arguments
+(defn one-less-arg [f x]
+  (fn [& args] (apply f x args)))
+
+;; 13) Define a function two-fns which takes two functions as arguments, f and g. It returns another function which takes one argument, calls g on it, then calls f on the result, and returns that.
+;; That is, your function returns the composition of f and g.
+
+(defn two-fns [f g]
+  (fn [x] (f (g x))))
+
+
 ;; Cannot nest Anonymous functions since that would create ambiguity as the parameters are not named
 
 ;; This is valid
